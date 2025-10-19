6f70b0868305bdf7941974bd59cfd889b73f555b
onisama3k <iambestsv@gmail.com>
2025-10-19 15:01:37 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6cgTn-iTgzdXZypXSANISEJ-442I9pnA",
+  authDomain: "bai-tap-lon-8858b.firebaseapp.com",
+  projectId: "bai-tap-lon-8858b",
+  storageBucket: "bai-tap-lon-8858b.firebasestorage.app",
+  messagingSenderId: "460161910501",
+  appId: "1:460161910501:web:b805013d2c0a2f88aff60c",
+  measurementId: "G-Q7LGH5RJN0"
+};
+// Khởi tạo Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
