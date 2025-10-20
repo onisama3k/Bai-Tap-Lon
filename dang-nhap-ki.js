c7d64ef5a14a59a34b228a01e814b66f2a9d5070
onisama3k <iambestsv@gmail.com>
2025-10-20 10:03:28 +0700

    update html, css, js

---
+++ dang-nhap-ki.js
@@ -0,0 +1,115 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { auth, db } from "./firebase-config.js";
+//hàm kiểm tra cho form đăng ký
+async function validateRegisterForm(event) {
+    //1. Ngăn chặn hành vi gửi mặc định của form
+    //Hành động này sẽ giúp js có thời gian kiểm tra dữ liệu trước khi trang bị tải lại
+    event.preventDefault();
+    // lấy các phần tử HTML và lấy giá trị nhập vào
+    const username = document.getElementById("RegisterUsername").value.trim();
+    const password = document.getElementById("RegisterPassword").value.trim();
+    const repassword = document.getElementById("RegisterRePassword").value.trim();
+    const email = document.getElementById("RegisterEmail").value.trim();
+    const phonenumber = document.getElementById("RegisterPhoneNumber").value.trim();
+    const address = document.getElementById("RegisterAddress").value.trim();
+    const errorDisplay = document.getElementById("RegisterErrorMessages");
+    // reset thông báo lỗi trước khi kiểm tra
+    errorDisplay.innerHTML = '';
+    let errors = [];
+    // kiểm tra username
+    if(username === "" || password === "" || repassword === "" || phonenumber === "" || address === "" || email === ""){
+        errors.push("Vui lòng điền đầy đủ thông tin.");
+    }
+    if( username.length < 6 || username.length > 15 || !/^[a-zA-Z0-9]+$/.test(username)){
+        errors.push("Tên đăng nhập phải có từ 6 đến 15 ký tự và chứa chỉ chứa chữ cái và số.");
+    }
+    // kiểm tra password
+    if( password.length <6 || password.length > 15 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test(password)){
+        errors.push("Mật khẩu phải có từ 6 đến 15 ký tự  và bao gồm chữ hoa, chữ thường và số.");
+    }
+    // kiếm tra repassword
+    if(password !== repassword){
+        errors.push("Mật khẩu không khớp.");
+    }
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        errors.push("Email không hợp lệ.");
+    }
+    // kiểm tra số điện thoại
+    if(!/^(0[0-9]{9}$)/.test(phonenumber)){
+        errors.push("Số điện thoại không hợp lệ.");
+    }
+    if(address.length < 10){
+        errors.push("Vui lòng nhập địa chỉ đầy đủ.");
+    }
+    if (errors.length > 0) {
+    const box = document.getElementById("errorBox");
+    errorDisplay.innerHTML = ''; // reset
+    errors.forEach(err => {
+       errorDisplay.innerHTML += '<p>' + err + '</p>';
+    });
+    box.style.display = "flex"; //  bật hiển thị lại hộp
+    return false;
+    }
+else {
+    document.getElementById("errorBox").style.display = "none";
+
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        await setDoc(doc(db, "users", userCredential.user.uid), {
+            username: username,
+            email: email,
+            phone: phonenumber,
+            address: address,
+            createdAt: new Date()
+        });
+
+        alert("Đăng ký thành công!");
+        window.location.href = "dang-nhap.html";
+    } catch (error) {
+        showError(error.message);
+    }
+}
+}
+//Hàm kiểm tra cho form đăng nhập
+function validateLoginForm(event) {
+    event.preventDefault();
+    const username = document.getElementById("LoginUsername").value.trim();
+    const password = document.getElementById("LoginPassword").value.trim();
+    const email = document.getElementById("LoginEmail").value.trim();
+    const errorDisplay = document.getElementById("LoginErrorMessages");
+    
+    let errors = [];
+    errorDisplay.innerHTML = '';
+    if(email === "" || password === ""){
+        errors.push("Vui lòng điền đầy đủ email và mật khẩu.");
+    }
+    if(errors.length > 0){
+       const box = document.getElementById("errorBox");
+    errorDisplay.innerHTML = ''; // reset
+    errors.forEach(err => {
+       errorDisplay.innerHTML += '<p>' + err + '</p>';
+    });
+    box.style.display = "flex"; //  bật hiển thị lại hộp
+    return false;
+    } else {
+        alert("Đăng nhập thành công!");
+        return false;
+    }
+}
+function showError(message) {
+  const box = document.getElementById("errorBox");
+  const msg = document.getElementById("RegisterErrorMessages");
+  
+  msg.textContent = "❗ " + message;  // đổi nội dung lỗi
+  box.style.display = "flex";         // hiển thị lại hộp lỗi
+}
+function showErrorLogin(message) {
+  const box = document.getElementById("errorBox");
+  const msg = document.getElementById("LoginErrorMessages");
+  msg.innerHTML = "❗ " + message;
+  box.style.display = "flex";
+}
+function closeErrorBox() {
+  document.getElementById("errorBox").style.display = "none";
+}
--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyC6cgTn-iTgzdXZypXSANISEJ-442I9pnA",
   authDomain: "bai-tap-lon-8858b.firebaseapp.com",
