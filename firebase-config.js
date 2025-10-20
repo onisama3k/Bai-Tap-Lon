79feb000882f6eb539594f09ff4a1d064d29d7b0
onisama3k <iambestsv@gmail.com>
2025-10-20 10:52:58 +0700

    update js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC6cgTn-iTgzdXZypXSANISEJ-442I9pnA",
   authDomain: "bai-tap-lon-8858b.firebaseapp.com",
   projectId: "bai-tap-lon-8858b",
-  storageBucket: "bai-tap-lon-8858b.appspot.com",
+  storageBucket: "bai-tap-lon-8858b.firebasestorage.app",
   messagingSenderId: "460161910501",
   appId: "1:460161910501:web:b805013d2c0a2f88aff60c",
   measurementId: "G-Q7LGH5RJN0"
 };
-// Khởi tạo Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
